bdb49bc959700c8673845b5ed8d795cdd82cdbf7
Martín Calvo Pena <MCP@gadisa.es>
2018-09-27 12:48:52 +0200

    CAmbios en sistema de ficheros

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$options = [
+    "location" => "http://soap-service.server/index.php",
+    "uri" => "http://soap-service.server/index.php",
+    "trace" => true
+];
+
+//sin WSDL
+//$client = new SoapClient($wsdl = null, $options);
+//con WSDL
+$client = new SoapClient("http://soap-service.server/wsdl.xml", $options);
+
+print($client->saludarCliente());
+print($client->saludarUsuario());
+
+?>
